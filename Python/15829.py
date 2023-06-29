5c13178cbd27659f088f75eaedf11d1f0e04381f
DoSeungJae <dsj5508@naver.com>
2023-06-29 12:13:21 +0900

    Hashing

---
+++ Python/15829.py
@@ -0,0 +1,16 @@
+import sys
+input=sys.stdin.readline
+m=1234567891
+r=31
+
+
+l=int(input())
+string=input().strip()
+
+h=0
+for i in range(l):
+    h=h+((ord(string[i])-96)*(31**i)%m)
+
+print(h)
+
+
