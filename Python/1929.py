f3cdbc8a598531f04fd6e1d2cdca90f60405aed5
DoSeungJae <dsj5508@naver.com>
2023-07-06 13:34:43 +0900

    소수 구하기

---
+++ Python/1929.py
@@ -0,0 +1,26 @@
+import sys
+input=sys.stdin.readline
+
+m,n=map(int,input().split())
+
+def prime_list(n):
+   
+    sieve = [True] * n
+
+    m = int(n ** 0.5)
+    for i in range(2, m + 1):
+        if sieve[i] == True:           
+            for j in range(i+i, n, i): 
+                sieve[j] = False
+
+  
+    return [i for i in range(2, n) if sieve[i] == True]
+
+
+n_list=prime_list(n+1)
+
+for i in n_list:
+    if(i>=m):
+        print(i)
+
+        
