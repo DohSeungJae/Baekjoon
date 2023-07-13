69cb3be5dbb46c84eaeb99d4cbd30e7fd9b006d7
DoSeungJae <dsj5508@naver.com>
2023-07-13 16:39:47 +0900

    마인크래프트

--- Python/18111.py
+++ Python/18111.py
@@ -19,13 +19,13 @@ for h in range(257):
 
     for i in range(n):
         for j in range(m):
-            diff=block[i][j]-h #양수이면 block이 더 높음, 음수이면 기준 높이(h)가 더 높음
+            diff=block[i][j]-h 
             
-            if(diff>0): #현재 block이 더 높을 때 > 내려가야함. 인벤토리 공간.
+            if(diff>0): 
                 inven+=diff
                 down+=diff
                 
-            else: ##현재 block이 더 낮을 때 > 올라가야함. /혹은 기준높이와 현재높이가 같을때 
+            else: 
                 diff=-1*diff
                 inven-=diff
                 up+=diff
