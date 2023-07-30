8cbbf65ea3a16cb811ad0b9734dac2df1597b24f
DoSeungJae <dsj5508@naver.com>
2023-07-30 14:22:20 +0900

    제곱수의 합

---
+++ Python/1699.py
@@ -0,0 +1,28 @@
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+
+sq=[0,1,2,3,1]
+if(n<5):
+    print(sq[n])
+    exit(0)
+
+for i in range(5,n+1):
+    root=int(i**0.5)
+    if(root*root==i):
+        sq.append(1)
+    else:
+        temp=[]
+        maxSq=1
+        while maxSq**2<=i:
+            cand=sq[maxSq**2]+sq[i-maxSq**2]
+            temp.append(cand)
+            maxSq+=1
+
+        sq.append(min(temp))
+
+print(sq[n])
+
+#제곱수의 합
+#17626 Four Squares와 동일
