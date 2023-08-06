f302912a9e1971339b95c41c8bee70a92d0e3a78
DoSeungJae <dsj5508@naver.com>
2023-08-06 12:51:35 +0900

    카잉 달력

---
+++ Python/6064.py
@@ -0,0 +1,21 @@
+import sys
+input=sys.stdin.readline
+
+t=int(input())
+
+for _ in range(t):
+    m,n,x,y=map(int,input().split())
+
+    b=0
+    while (x<m*n):
+        if(x%n==y%n):
+            b=x
+            break
+        else:
+            x+=m
+
+    if(b==0):
+        print(-1)
+    else:
+        print(b)
+        
