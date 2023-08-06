c5944c08c9a9d681a5a8d7b54fd45e089ae161e6
DoSeungJae <dsj5508@naver.com>
2023-08-06 14:09:29 +0900

    가장 가까운 세 사람의 심리적 거리

--- Python/20529.py
+++ Python/20529.py
@@ -7,16 +7,18 @@ for _ in range(t):
     mbti=list(map(str,input().strip().split()))
     dist_min=sys.maxsize
 
+    if(n>32):
+        print(0)
+        continue
+
     for i in range(n-2):
         for j in range(i+1,n-1):
             for k in range(j+1,n):
                 dist=0
                 for idx in range(4):
-                    if(mbti[i][idx]!=mbti[j][idx] ): dist+=1
+                    if(mbti[i][idx]!=mbti[j][idx]): dist+=1
                     if(mbti[j][idx]!=mbti[k][idx]): dist+=1
                     if(mbti[i][idx]!=mbti[k][idx]): dist+=1
-
-                
 
                 if(dist<dist_min):
                     dist_min=dist
