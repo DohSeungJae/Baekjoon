f488ee87ec489a7c9e80bf5548982495e3da9753
DoSeungJae <dsj5508@naver.com>
2023-08-10 14:12:37 +0900

    리코먼

--- Python/1107.py
+++ Python/1107.py
@@ -7,8 +7,8 @@ if m!=0: m_list=list(map(int,input().split()))
 else: m_list=[]
 
 default=100
-#1000001
 minChDiff=sys.maxsize
+minCh=0
 for ch in range(1000001):
     b=1
     for i in str(ch):
@@ -23,11 +23,4 @@ for ch in range(1000001):
         minCh=ch
 
 res=min(abs(default-n),minChDiff+len(str(minCh)))
-print(minChDiff,len(str(minCh)))
-print(minCh)
-
 print(res)
-
-
-
-
