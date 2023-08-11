bd8925c6ea354ee8ec2d16396ba278487d1dd8e5
DoSeungJae <dsj5508@naver.com>
2023-08-11 14:49:39 +0900

    import sys

---
+++ Python/15650.py
@@ -0,0 +1,25 @@
+import sys
+input=sys.stdin.readline
+
+
+n,m=map(int,input().split())
+stack=[]
+
+def backTracking():
+    if len(stack)==m:
+        b=1
+        for i in range(m-1):
+           if(not stack[i]<stack[i+1]):
+               b=0
+
+        if b: print(*stack)
+         
+
+        return 
+    for i in range(1,n+1):
+        if i not in stack:
+            stack.append(i)
+            backTracking()
+            stack.pop()
+
+backTracking()
