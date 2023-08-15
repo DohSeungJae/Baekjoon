bb5f3532b1ffa6c0a119a397a731418ae95fb885
DoSeungJae <dsj5508@naver.com>
2023-08-15 14:57:02 +0900

    N과M(5)

---
+++ Python/15654.py
@@ -0,0 +1,28 @@
+import sys
+input=sys.stdin.readline
+
+n,m=map(int,input().split())
+n_list=list(map(int,input().split()))
+n_list.sort()
+stack=[]
+visitDict={}
+for i in n_list:
+    visitDict[i]=0
+
+def backTracking(start):
+    if(len(stack)==m):
+        print(*stack)
+        return 
+    
+    for i in range(n):
+        num=n_list[i]
+        if visitDict[num]==0:
+            visitDict[num]=1
+            stack.append(num)
+            backTracking(num)
+            stack.pop()
+            visitDict[num]=0
+
+
+
+backTracking(n_list[0])
