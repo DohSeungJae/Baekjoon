0c60623ef495add47b11a3a3cf29f36f814d9340
DoSeungJae <dsj5508@naver.com>
2023-08-18 16:41:33 +0900

    알파벳

---
+++ Python/1987.py
@@ -0,0 +1,30 @@
+
+n,m=map(int,input().split())
+graph=[]
+for _ in range(n):
+    graph.append(list(input().strip()))
+
+
+max_path=-1
+visit=set()
+nextX=[-1,1,0,0]
+nextY=[0,0,-1,1]
+
+def dfs(x,y,cnt):
+    global max_path
+    max_path=max(max_path,cnt)
+    
+    for i in range(4):
+        nx=x+nextX[i]
+        ny=y+nextY[i]
+
+        if((0<=nx<m and 0<=ny<n) and graph[ny][nx] not in visit):
+            visit.add(graph[ny][nx])
+            dfs(nx,ny,cnt+1)
+            visit.remove(graph[ny][nx])
+
+
+visit.add(graph[0][0])      
+dfs(0,0,1)
+print(max_path)
+
