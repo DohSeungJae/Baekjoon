7297c1f77e79975715ac4212643af805e368cb17
DoSeungJae <dsj5508@naver.com>
2023-08-20 10:29:20 +0900

    부울행렬의 부울곱

---
+++ Python/14492.py
@@ -0,0 +1,18 @@
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+mat1=[list(map(int,input().split())) for _ in range(n)]
+mat2=[list(map(int,input().split())) for _ in range(n)]
+
+cnt=0
+for i in range(n):
+    for j in range(n):
+        c=0
+        for h in range(n):
+            c+=mat1[i][h]*mat2[h][j]
+        
+        if(c):
+            cnt+=1
+
+print(cnt)
