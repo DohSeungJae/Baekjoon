f03c65c0d5a3aa28ffaabf9f21038964a8eb12ae
DoSeungJae <dsj5508@naver.com>
2023-08-25 12:15:06 +0900

    가장 긴 감소하는 부분 수열

---
+++ Python/11722.py
@@ -0,0 +1,15 @@
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+n_list=[sys.maxsize]
+n_list+=list(map(int,input().split()))
+dp=[0]*(n+10)
+
+for i in range(1,n+1):
+    for j in range(i):
+        if(n_list[i]<n_list[j]):
+            dp[i]=max(dp[i],dp[j]+1)
+
+
+print(max(dp))
