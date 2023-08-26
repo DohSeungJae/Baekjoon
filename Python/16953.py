bdfd8b7064cc0be07af1df2fa516cfb5b033a55d
DoSeungJae <dsj5508@naver.com>
2023-08-26 10:21:49 +0900

    A -> B

---
+++ Python/16953.py
@@ -0,0 +1,26 @@
+import sys
+input=sys.stdin.readline
+
+a,b=map(int,input().split())
+
+cnt=1
+while a<b:
+    if(str(b)[-1]=='1'):
+        b=b//10
+        
+    elif(b%2==0):
+        b=b//2
+    
+    else:
+        break
+    
+    
+    cnt+=1
+    print(b)
+
+if(a==b):
+    print(cnt)
+else:
+    print(-1)
+
+print(a,b)
