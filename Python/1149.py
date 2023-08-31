0b07ddeaae34054f3074547fad03eb266ecb07cf
DoSeungJae <dsj5508@naver.com>
2023-08-31 12:15:49 +0900

    RGB거리

--- Python/1149.py
+++ Python/1149.py
@@ -3,10 +3,9 @@ input=sys.stdin.readline
 
 n=int(input())
 cost_rgb=[[0,0,0]]
-for _ in range(n):
-    cost_rgb.append(list(map(int,input().split())))
-
 for i in range(1,n+1):
+    cost_rgb.append(list(map(int,input().split())))
+    
     cost_rgb[i][0]+=min(cost_rgb[i-1][1],cost_rgb[i-1][2])
     cost_rgb[i][1]+=min(cost_rgb[i-1][0],cost_rgb[i-1][2])
     cost_rgb[i][2]+=min(cost_rgb[i-1][1],cost_rgb[i-1][0])
