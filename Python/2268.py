9e2f9ca0327a2c6a3d8fccb230fa153f60c5b8fb
DoSeungJae <dsj5508@naver.com>
2023-09-09 13:57:49 +0900

    수들의 합7

---
+++ Python/2268.py
@@ -0,0 +1,60 @@
+import sys
+input=sys.stdin.readline
+
+class SegmentTree:
+    def __init__(self,arr):
+        self.n=len(arr)
+        self.tree=[0] * (4*self.n) #?
+        self.lazy=[0] * (4*self.n)
+        self.build(1,0,self.n-1,arr)
+    
+    def build(self,node,left,right,arr):
+        if(left==right):
+            self.tree[node]=arr[left]
+            return 
+
+        mid=(left+right)//2
+        self.build(2*node,left,mid,arr)
+        self.build(2*node+1,mid+1,right,arr)
+        self.tree[node]=self.tree[2*node]+self.tree[2*node+1]
+    
+    def query(self,ql,qr,node,left,right):
+        if left > qr or right < ql:
+            return 0
+        
+        if(ql<=left and right<=qr):
+            return self.tree[node] 
+        
+        mid=(left+right)//2
+        left_sum=self.query(ql,qr,2*node,left,mid)
+        right_sum=self.query(ql,qr,2*node+1,mid+1,right)
+        return left_sum+right_sum
+    
+    def update(self,idx,value,node,left,right):
+        if left==right:
+            self.tree[node]=value
+            return 
+        
+        mid=(left+right)//2
+        if(left<=idx<=mid):
+            self.update(idx,value,node*2,left,mid)
+        else:
+            self.update(idx,value,node*2+1,mid+1,right)
+        
+        self.tree[node]=self.tree[2*node]+self.tree[2*node+1]
+
+if __name__=="__main__":
+    n,m=map(int,input().split())
+    arr=[0 for _ in range(n)]
+    seg_tree=SegmentTree(arr)
+
+    for _ in range(m):
+        cmd,a,b=map(int,input().split())
+        if(cmd):
+            seg_tree.update(a-1,b,1,0,len(arr)-1)
+        else:
+            if a>b:
+                a,b=b,a
+            print(seg_tree.query(a-1,b-1,1,0,len(arr)-1))
+
+    
