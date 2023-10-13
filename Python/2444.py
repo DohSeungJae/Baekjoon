6e57a07b04d870f1b7d09248fbc513ffbb93439b
DoSeungJae <dsj5508@naver.com>
2023-10-13 16:16:00 +0900

    별 찍기 - 7

--- Python/2444.py
+++ Python/2444.py
@@ -7,9 +7,7 @@ for i in range(n):
     for j in range(1+2*i):
         print("*",end="")
 
-
     print()
-
 
 for i in range(n-2,-1,-1):
     for j in range(n-i-2,-1,-1):
@@ -17,7 +15,5 @@ for i in range(n-2,-1,-1):
 
     for j in range(1+2*i):
         print("*",end="")
-
-
 
     print()
