5953962072a8a7a8dd957fb3f65fe1ac45d3d773
DoSeungJae <dsj5508@naver.com>
2023-10-17 20:27:21 +0900

    세로 읽기

--- Python/10798.py
+++ Python/10798.py
@@ -1,4 +1,15 @@
 table=[]
+maxLen=0
 for _ in range(5):
-    table.append(list)
+    temp=str(input())
+    maxLen=max(maxLen,len(temp))
+    table.append(temp)
+
+for idx in range(maxLen):
+    for line in range(5):
+        curLen=len(table[line])
+        if idx>curLen-1:
+            continue
+        print(table[line][idx],end="")
+
 
