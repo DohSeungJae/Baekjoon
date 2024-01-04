fa7bbd013299a4ee1da830bdcd9407a4640340ed
DoSeungJae <dsj5508@naver.com>
2024-01-04 15:33:31 +0900

    특정한 최단 경로

--- Python/1504.py
+++ Python/1504.py
@@ -1,7 +1,7 @@
 import sys
 import heapq
 input=sys.stdin.readline
-
+INF=sys.maxsize
 
 N,E=map(int,input().split())
 graph=[[] for _ in range(N+1)]
@@ -13,7 +13,7 @@ for _ in range(E):
 v1,v2=map(int,input().split())
 
 def dijkstra(start:int,dest:int)->int:
-    minCost=[0xffffff for _ in range(N+1)]
+    minCost=[INF for _ in range(N+1)]
     minCost[0]=-1
     minCost[start]=0
 
@@ -27,14 +27,14 @@ def dijkstra(start:int,dest:int)->int:
             cost=minCost[cur]+next[1]
             if(cost<minCost[next[0]]):
                 minCost[next[0]]=cost
-            heapq.heappush(q,(cost,next[0]))
+                heapq.heappush(q,(cost,next[0]))
 
     return minCost[dest]
 
 way1=dijkstra(1,v1)+dijkstra(v1,v2)+dijkstra(v2,N)
 way2=dijkstra(1,v2)+dijkstra(v2,v1)+dijkstra(v1,N)
 
-if(way1>=0xffffff or way2>=0xffffff):
+if(way1>=INF or way2>=INF or dijkstra(1,N)>=INF):
     print(-1)
 else:
     print(min(way1,way2))
