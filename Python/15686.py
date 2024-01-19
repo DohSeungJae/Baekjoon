3c963c7b8477d776e87c14137524643245420da9
DoSeungJae <dsj5508@naver.com>
2024-01-19 16:04:49 +0900

    15686 - 치킨 배달

--- Python/15686.py
+++ Python/15686.py
@@ -1,19 +1,57 @@
 import sys
-from itertools import combinations
+sys.setrecursionlimit(10**6)
 input=sys.stdin.readline
 
-n,m=map(int,input().split())
-board=[]
-cnt2=0
-cords2=[]
-for _ in range(n):
-    temp=list(map(int,input().split()))
-    if 2 in temp:
-        for i in range(n):
+N,M=map(int,input().split())
+graph=[]
+cordinateOf2=[]
+cordinateOf1=[]
 
-elements = [i for i in range(1, m+1)]  # 예: [1,2,3,4,5]
-  
-for combo in combinations(elements, n):
-    print(combo)
+for y in range(N):
+    line=list(map(int,input().split()))
+    if 2 in line:
+        for x in range(len(line)):
+            if(line[x]==2):
+                cordinateOf2.append((y,x))
+    if 1 in line:
+        for x in range(len(line)):
+            if(line[x]==1):
+                cordinateOf1.append((y,x))
+    graph.append(line)
 
-        
+minDist=sys.maxsize
+stack=[]
+cntOf2=len(cordinateOf2)
+cnt=0
+
+def remove2(start:int):
+    global cnt
+    condition=cntOf2-len(stack)
+    if(M==condition):
+        cnt+=1
+        calculateDistance(stack)
+        return 
+    for i in range(start,len(cordinateOf2)):
+        (y,x)=cordinateOf2[i]
+        if((y,x) not in stack):
+            stack.append((y,x))
+            remove2(i+1)
+            stack.pop()
+
+def calculateDistance(stack:list)->None:
+    global minDist
+    distance={}
+    for fr in cordinateOf2:
+        if(fr in stack):
+            continue
+        for to in cordinateOf1:
+            if(to not in distance.keys()):
+                distance[to]=sys.maxsize
+            distance[to]=min(distance[to],abs(fr[0]-to[0])+abs(fr[1]-to[1]))
+    sumOfDistances=0
+    for i in distance.keys():
+        sumOfDistances+=distance[i]
+    minDist=min(minDist,sumOfDistances)
+
+remove2(0)
+print(minDist)
