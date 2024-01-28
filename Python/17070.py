974baee7dd36f7a4a7416210201dc7d05852aff0
DoSeungJae <dsj5508@naver.com>
2024-01-28 17:13:55 +0900

    17070 - 파이프 옮기기 1

---
+++ Python/17070.py
@@ -0,0 +1,57 @@
+import sys
+input=sys.stdin.readline
+
+def dfs(cord1:list,curStatus:int)->None: ##dfs?? backTracking??
+    global cnt
+    if(cord1==[N-1,N-1]):
+        cnt+=1
+        return 
+    for nextStatus in range(3):
+        if(nextStatus==0):
+            nextX=cord1[1]+1
+            nextY=cord1[0]
+            if(curStatus==2):
+                continue
+            elif(nextX==N):
+                continue
+            elif(graph[nextY][nextX]==1):
+                continue
+
+        elif(nextStatus==1):
+            nextX=cord1[1]+1
+            nextY=cord1[0]+1
+            if(nextX==N or nextY==N):
+                continue
+            elif(graph[nextY][nextX]==1 or \
+               graph[nextY-1][nextX]==1 or graph[nextY][nextX-1]==1):
+                continue
+
+        else:
+            nextX=cord1[1]
+            nextY=cord1[0]+1
+            if(curStatus==0):
+                continue
+            elif(nextY==N):
+                continue
+            elif(graph[nextY][nextX]==1):
+                continue
+        
+        dfs([nextY,nextX],nextStatus)
+
+N=int(input())
+cord1=[0,1]
+
+status=0
+graph=[]
+cnt=0
+
+for _ in range(N):
+    line=list(map(int,input().split()))
+    graph.append(line)
+
+if(graph[N-1][N-1]==1):
+    print(0)
+    exit(0)
+
+dfs(cord1,status)
+print(cnt)
