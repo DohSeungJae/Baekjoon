dd40789a7baa15d9ea721636e21dac9cf32bdcfb
DoSeungJae <dsj5508@naver.com>
2024-02-03 20:02:54 +0900

    [WRONG] 1967 - 트리의 지름

--- Python/1967.py
+++ Python/1967.py
@@ -9,30 +9,29 @@ for _ in range(n-1):
     p,c,w=map(int,input().split())
     tree[p].append((c,w))
 
-stackA=[0]
-maxDiameter=0 
+stack=[0]
 
 def backTracking(curNode:int,weight:int)->None:
     print(curNode)
-    
-    global maxDiameter
+
+    diameter=0
     if(tree[curNode]==[]): #이 조건문은 필요없는 것 같다
         return weight #가중치를 return해서 상위 함수에서 합산하도록 처리
     stack=[] #자식들의 가중치 값을 저장한 스택 
     for child in tree[curNode]:
         stack.append(backTracking(child[0],child[1]))
         #상위 함수로 lineWeight를 반환해야함.
+    stack.sort(reverse=True)
     if(len(stack)>=2):
-        stack.sort(reverse=True)
-        maxDiameter=max(stack[0]+stack[1],stack[0]+weight)
-    if(maxDiameter>stackA[-1]):
-        stackA.append(maxDiameter)
+        diameter=max(stack[0]+stack[1],stack[0]+weight)
+    if(diameter>stack[-1]):
+        stack.append(diameter)
     return weight+stack[0]
 
 
 backTracking(1,0)
-print(stackA)
-print(maxDiameter)
+print(stack)
+
 
 #backTracking함수의 시작(루트)노드는 1
 #backTracking(1,0,0,[])
