7959d2f6cb0bd9526fea65fcc41f6963454dd119
DoSeungJae <dsj5508@naver.com>
2024-02-08 13:51:19 +0900

    [SOLVED] 31090 - '2023은 무엇이 특별할까?'

---
+++ Python/31090.py
@@ -0,0 +1,11 @@
+import sys
+input=sys.stdin.readline
+
+N=int(input())
+
+for _ in range(N):
+    year=int(input())
+    if((year+1)%(year%100)==0):
+        print("Good")
+    else:
+        print("Bye")
