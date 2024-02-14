54e608a910dec19f483ba7c6cb35a5e5203ce054
DoSeungJae <dsj5508@naver.com>
2024-02-14 17:46:34 +0900

    [SOVLED] 13913 - '숨바꼭질 4'

---
+++ Python/13913.py
@@ -0,0 +1,40 @@
+import sys
+from collections import deque
+input=sys.stdin.readline
+sys.setrecursionlimit(10**5)
+
+visited=[0]*100001
+minCost=[0]*100001
+vertexFrom={}
+
+N,K=map(int,input().split())
+def bfs(v):
+    q=deque()
+    q.append(v)
+    visited[v]=1
+
+    while len(q)>0:
+        v=q.popleft()
+        if(v==K):
+            break
+        for next in (v-1,v+1,2*v):
+            if(next>100000 or next<0):
+                continue
+            if(visited[next]==0):
+                visited[next]=1
+                minCost[next]=minCost[v]+1
+                q.append(next)
+                if(next not in vertexFrom.keys()):
+                    vertexFrom[next]=v
+
+def printPath(cur:int):
+    if(cur==N):
+        print(cur,end=" ")
+        return 
+    printPath(vertexFrom[cur])
+    print(cur,end=" ")
+
+
+bfs(N)
+print(minCost[K])
+printPath(K)
