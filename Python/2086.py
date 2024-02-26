be34cf619256484e6adf885d65556af4043e427b
DoSeungJae <dsj5508@naver.com>
2024-02-26 11:50:23 +0900

    [AC] 2086 - '피보나치 수의 합 '

--- Python/2086.py
+++ Python/2086.py
@@ -16,32 +16,20 @@ def matrixMultiple(a:list,b:list)->list:
 
 def matrixPower(a:list,b:int)->list:
     if(b==1):
-        returnValue=[[x%MOD for x in row] for row in a]
+        return [[x%MOD for x in row] for row in a]
     elif(b%2==0):
         temp=matrixPower(a,b//2)
-        returnValue= [[x%MOD for x in row] for row in matrixMultiple(temp,temp)]
+        return [[x%MOD for x in row] for row in matrixMultiple(temp,temp)]
     else:
         temp=matrixPower(a,b//2)
-        returnValue= [[x%MOD for x in row] \
+        return [[x%MOD for x in row] \
                 for row in matrixMultiple(a,matrixMultiple(temp,temp))]
-        
-    if((b) not in table.keys()):
-        table[b]=returnValue[0][1]
-    return returnValue
 
 
-start,end=map(int,input().split())
-res=0
+a,b=map(int,input().split())
 
-for n in range(end,start-1,-1):
-    if(n==0):
-        continue 
-    elif(n==1 or n==2):
-        res+=1
-    else:
-        if(n in table.keys()):
-            res+=table[n]
-        else:
-            res+=matrixPower(F,n-1)[0][0]
 
-print(res)
+
+print(matrixPower(F,b+2)[0][1]-matrixPower(F,a+1)[0][1])
+print(matrixPower(F,b)[0][1])
+
