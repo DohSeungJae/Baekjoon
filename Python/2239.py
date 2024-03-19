11bfb68b9d1974483042aaefdc34c6b81e3fa5da
DoSeungJae <dsj5508@naver.com>
2024-03-19 12:58:32 +0900

    [REFERED] 2239 - '스도쿠'

--- Python/2239.py
+++ Python/2239.py
@@ -1,79 +1,78 @@
-import sys
-input=sys.stdin.readline
 
+class SudokuSolver:
+    def solve(self,b:list)->None:
+        self.__board=b
+
+        next_row,next_col=self.__find_next_empty()
+        if(next_row==9 and next_col==9):
+            return 
+        
+        for i in range(1,9):
+            self.__bt(next_row,next_col,i)
+
+    def __check_row(self,row:int,num:int)->bool:
+        for x in range(9):
+            if(self.__board[row][x]==num):
+                return False
+        return True
+        
+    def __check_col(self,col:int,num:int)->bool:
+        for y in range(9):
+            if(self.__board[y][col]==num): #!!!!
+                return False
+        return True
+        
+    def __check_33(self,row:int,col:int,num:int)->bool:
+        box_x=int(col/3)*3
+        box_y=int(row/3)*3
+        for y in range(box_y,box_y+3):
+            for x in range(box_x,box_x+3):
+                if(self.__board[y][x]==num):
+                    return False
+        return True
+    
+    def __find_next_empty(self):
+        for y in range(0,9):
+            for x in range(0,9):
+                if(self.__board[y][x]==0):
+                    return [y,x]
+        return [9,9]
+        
+    def __bt(self,row:int,col:int,num:int)->bool:
+        if(not self.__check_row(row,num)):
+            return False
+        elif(not self.__check_col(col,num)):
+            return False
+        elif(not self.__check_33(row,col,num)):
+            return False
+
+        self.__board[row][col]=num
+        next_row,next_col=self.__find_next_empty()
+
+        if(next_row==9 and next_col==9):
+            return True
+            
+        for next_num in range(1,10):
+            if(self.__bt(next_row,next_col,next_num)):
+                return True
+            
+        self.__board[row][col]=0
+        return False
+        
 board=[]
-zeroes=[]
-isDefinedRow=[{i+1:False for i in range(9)} for _ in range(9)] 
-isDefinedColumn=[{i+1:False for i in range(9)} for _ in range(9)]
-isDefinedBox=[{i+1:False for i in range(9)} for _ in range(9)]  
+solver=SudokuSolver()
 
 for y in range(9):
     line=input().strip()
     temp=[int(i) for i in line]
     for x in range(len(temp)):
         integer=temp[x]
-        if(integer==0):
-            zeroes.append([y,x])
-        else:
-            isDefinedRow[x][integer]=True
-            isDefinedColumn[y][integer]=True
-            idxOfBox=(x//3)+(y//3)*3
-            isDefinedBox[idxOfBox][integer]=True
     
     board.append(temp)
 
-stack=[]
-def backTracking(start:int)->None:
-    if(start<=25):
-        print(start)
-
-        for line in board:
-            for i in line:
-                print(i,end="")
-            print()
-        print()
-
-
-    if(len(stack)==len(zeroes)):
-        for line in board:
-            for i in line:
-                print(i,end="")
-            print()
-        print()
-        return 
-    
-    for i in range(start,len(zeroes)):
-        y,x=zeroes[i][0],zeroes[i][1]
-        idxOfBox=(x//3)+(y//3)*3
-        if([y,x] in stack):
-            continue
-        adopted=0
-        for integer in range(1,9+1):
-            if(isDefinedRow[x][integer]):
-                continue
-            if(isDefinedColumn[y][integer]):
-                continue
-            if(isDefinedBox[idxOfBox][integer]):
-                continue
-            adopted=integer
-
-            stack.append([y,x])
-            isDefinedRow[x][adopted]=True
-            isDefinedColumn[y][adopted]=True
-            isDefinedBox[idxOfBox][adopted]=True
-            board[y][x]=adopted
-            backTracking(i+1)
-            stack.pop()
-            isDefinedRow[x][adopted]=False
-            isDefinedColumn[y][adopted]=False
-            isDefinedBox[idxOfBox][adopted]=False
-            board[y][x]=0
-        if(adopted==0):
-            return 
-
-backTracking(0)
-
-
-    
-    
+solver.solve(board)
+for row in board:
+    for x in row:
+        print(x,end="")
+    print()
 
