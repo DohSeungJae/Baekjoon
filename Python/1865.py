b1f7df2cc4622e0d44a2867f046104e0c91e5cd3
DoSeungJae <dsj5508@naver.com>
2024-04-01 13:15:40 +0900

    [REFERED] 1865 -'웜홀'

--- Python/1865.py
+++ Python/1865.py
@@ -3,13 +3,10 @@ input=sys.stdin.readline
 INF=sys.maxsize
 
 def bf()->bool:
-    #start=1
-    #minCost[start]=0
-
     for i in range(N):
         for j in range(2*M+W):
             curNode,nextNode,cost=arr[j]
-            if( minCost[nextNode]>minCost[curNode]+cost):
+            if(minCost[nextNode]>minCost[curNode]+cost):
                 minCost[nextNode]=minCost[curNode]+cost
                 if(i==N-1):
                     return True
@@ -31,15 +28,7 @@ for _ in range(TC):
         S,E,T=map(int,input().split())
         arr.append((S,E,-T))
     
-    '''
-    for i in range(M+W):
-        S,E,T=map(int,input().split())
-        if(i>=M):
-            T=-T
-        else:
-            arr.append((E,S,T))
-        arr.append((S,E,T))
-    '''
+
     isNegativeCycle=bf()
     if(isNegativeCycle):
         print("YES")
