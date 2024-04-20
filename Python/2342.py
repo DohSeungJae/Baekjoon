a8c8fec2ec56620efd7e22364012c852dfa29460
DoSeungJae <dsj5508@naver.com>
2024-04-20 15:32:14 +0900

    [REFERED] 2342 - 'Dance Dance Revolution'

--- Python/2342.py
+++ Python/2342.py
@@ -1,36 +1,40 @@
 import sys
 input=sys.stdin.readline
+INF=sys.maxsize
 
 commands=list(map(int,input().split()))
-foots=[0,0]
-totalCost=0
+commands.pop()
+n=len(commands)
+dp=[[[INF for _ in range(5)] for _ in range(5)] for _ in range(n+1)]
+dp[-1][0][0]=0
 
-def getCost(foot:int,target:int)->int:
-    if(foot==target):
-        return 1
-    elif(foot==0):
+def getCost(prev:int, target:int)->int:
+    if(prev==0):
         return 2
-    if(abs(foot-target)==2):
+    elif(abs(target-prev)==2):
         return 4
+    elif(prev==target):
+        return 1
     return 3
-
-for target in commands:
-    if(target==0):
-        break
-    leftCost=getCost(foots[0],target)
-    rightCost=getCost(foots[1],target)
-    if(rightCost<=leftCost and foots[0]!=target):
-        totalCost+=rightCost
-        foots[1]=target
-    elif(leftCost<rightCost and foots[1]!=target):
-        totalCost+=leftCost
-        foots[0]=target
-
-    print(foots)
-
-print(totalCost)
-
-
-
-
     
+for i in range(n):
+    target=commands[i]
+    for fixedR in range(5):
+        for prevL in range(5):
+            cost=getCost(prevL,target)
+            dp[i][target][fixedR]=min(dp[i][target][fixedR],
+                                        dp[i-1][prevL][fixedR]+cost)
+    for fixedL in range(5):
+        for prevR in range(5):
+            cost=getCost(prevR,target)
+            dp[i][fixedL][target]=min(dp[i][fixedL][target],
+                                      dp[i-1][fixedL][prevR]+cost)
+
+minCost=sys.maxsize
+for l in range(5):
+    for r in range(5):
+        minCost=min(minCost,dp[len(commands)-1][l][r])
+
+print(minCost)
+print(dp[len(commands)-1])
+
