7f2601e2cf0892f48c4c7744253bce23adaf3425
DoSeungJae <dsj5508@naver.com>
2024-04-27 15:17:37 +0900

    [REFERED] 12920 - '평범한 배낭 2'

--- Python/12920.py
+++ Python/12920.py
@@ -1,35 +1,21 @@
-import sys
-input=sys.stdin.readline
-
 N,M=map(int,input().split())
-bag=[[0,0,0]]
-maxK=0
+
+dp=[0 for _ in range(M+1)]
+bag=[]
+
 for _ in range(N):
-    V,C,K=map(int,input().split())
-    maxK=max(maxK,K)
-    bag.append([V,C,K])
-
-dp=[[[0 for _ in range(maxK+1)] for _ in range(M+1)] for _ in range(N+1)]
-
-
-for n in range(1,N+1):
-    V=bag[n][0]
-    C=bag[n][1]
-    K=bag[n][2]
-    for m in range(1,M+1):
-        for k in range(1,maxK+1):
-            km=m*k
-            kc=C*k
-            if(km<V):
-                dp[n][m][k]=dp[n-1][m][k]
-            elif(k*V>m):
-                dp[n][m][k]=dp[n][m][k-1]
-            else:
-                dp[n][m][k]=max(dp[n-1][m][k],dp[n-1][m-k*V][k]+kc,dp[n][m][k-1])
-
-         
-
-print(dp[-1][-1][-1])
+    W,C,K=map(int,input().split()) 
+    i=1
+    while K>0:
+        m=min(i,K)
+        bag.append([W*m,C*m])
+        K-=i
+        i*=2
+        #왜 곱하기 2?
 
 
+for W,C in bag:
+    for i in range(M,W-1,-1):
+        dp[i]=max(dp[i],dp[i-W]+C)
 
+print(dp[M])
