7f72191a3dce15264d30eefb3dc9fe94abb1ff42
DoSeungJae <dsj5508@naver.com>
2024-04-30 15:03:42 +0900

    [AC] 11049 - '행렬 곱셈 순서'

--- Python/11049.py
+++ Python/11049.py
@@ -16,8 +16,7 @@ for diagonal in range(2,N):
     for d in range(N-diagonal):
         i,j=d,d+diagonal
         for distance in range(diagonal):
-            y=1+i+distance
-            x=y-1    
+            y,x=i+1+distance,i+distance
             dp[i][j]=min(dp[i][j],(dp[i][x]+dp[y][j]+(mat[i][0]*mat[y][0]*mat[j][1])))
 
 print(dp[0][N-1])
