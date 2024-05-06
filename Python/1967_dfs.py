7798fa16ef43cb422a15525e0494ca9f2a6ff0b9
DoSeungJae <dsj5508@naver.com>
2024-05-06 10:55:00 +0900

    [REFER] 1967_dfs.py

---
+++ Python/1967_dfs.py
@@ -0,0 +1,29 @@
+import sys
+input=sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+N=int(input())
+tree=[[] for _ in range(N+1)]
+
+for _ in range(N-1):
+    p,c,w=map(int,input().split())
+    tree[p].append((c,w))
+    tree[c].append((p,w))
+
+def dfs(curNode:int,curWeight:int)->None:
+    for nextNode,nextWeight in tree[curNode]:
+        if(visited[nextNode]!=-1):
+            continue
+        visited[nextNode]=curWeight+nextWeight
+        dfs(nextNode,curWeight+nextWeight)
+
+visited=[-1]*(N+1)
+visited[1]=0
+dfs(1,0)
+
+extremeLeafNode=visited.index(max(visited))
+visited=[-1]*(N+1)
+visited[extremeLeafNode]=0
+dfs(extremeLeafNode,0)
+
+print(max(visited))
