319c1b7b025d0a888ece5f8b7a6e4749a445046c
DoSeungJae <dsj5508@naver.com>
2024-06-06 20:28:45 +0900

    [AC] 1160 - 'Random Number Generator'

--- Python/1160.py
+++ Python/1160.py
@@ -3,12 +3,8 @@ input=sys.stdin.readline
 LEN=2
 
 m,a,c,x0,n,g=map(int,input().split())
-a%=m
-c%=m
-x0%=m
 
 seq=[[a,0],[c,1]]
-seq2=[[x0,1],[0,0]]
 
 def matrixMultiple(a:list,b:list)->list:
     res=[[0 for _ in range(LEN)] for _ in range(LEN)]
@@ -18,8 +14,7 @@ def matrixMultiple(a:list,b:list)->list:
             for k in range(LEN):
                 res[i][j]+=(a[i][k]*b[k][j])
 
-
-    return [[x%m for x in row] for row in res]
+    return res
 
 def matrixPower(a:list,b:int)->list:
     if(b==1):
@@ -30,7 +25,6 @@ def matrixPower(a:list,b:int)->list:
     else:
         temp=matrixPower(a,b//2)
         return [[x%m for x in row] for row in matrixMultiple(a,matrixMultiple(temp,temp))]
-    
 
 resSeq=matrixPower(seq,n)
 print(((x0*resSeq[0][0]+resSeq[1][0])%m)%g)
