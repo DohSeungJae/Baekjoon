2336f9d83e0eeef257713c013a7f93801ffead0f
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2024-09-03 10:26:24 +0000

    [AC] 2847 - '게임을 만든 동준이'

--- Python/2847.py
+++ Python/2847.py
@@ -10,9 +10,9 @@ reduced=0
 for i in range(N-2,-1,-1):
     curScore=score[i]
     scoreLimit=score[i+1]
-    if(curScore>=scoreLimit):
-        reduced+=(curScore-scoreLimit+1)
-        score[i]=score[i]-(curScore-scoreLimit+1)
+    if(score[i]>=score[i+1]):
+        reduced+=(score[i]-score[i+1]+1)
+        score[i]-=(score[i]-score[i+1]+1)
     
 print(reduced)
 
