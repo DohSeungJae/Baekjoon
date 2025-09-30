90844fd889be97a9177c59d9ad7988a43c1918f9
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-09-30 10:44:37 +0000

    2025.09.30 - mst_kruskal.py

---
+++ Python/ndb/graph_theory/mst_kruskal.py
@@ -0,0 +1,40 @@
+import sys
+input=sys.stdin.readline
+
+def find_parent(parent,x):
+    if(parent[x]!=x):
+        parent[x]=find_parent(parent,parent[x])
+    return parent[x]
+
+def union_parent(parent,a,b):
+    a=find_parent(parent,a)
+    b=find_parent(parent,b)
+
+    if(a<b):
+        parent[b]=a
+    else:
+        parent[a]=b
+    
+v,e=map(int,input().split())
+
+parent=[i for i in range(v+1)]
+edges=[]
+for _ in range(e):
+    a,b,cost=map(int,input().split())
+    edges.append((cost,a,b))
+
+edges.sort()
+
+
+summ=0
+for (cost,a,b) in edges:
+    if(find_parent(parent,a)==find_parent(parent, b)): #사이클 생성
+        continue
+    union_parent(parent,a,b)
+    summ+=cost
+
+
+print(summ)
+
+
+
