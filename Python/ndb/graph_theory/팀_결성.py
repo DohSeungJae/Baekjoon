9e9d0eb1d249f21a53a8859f9ee8032be5231c76
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-10-01 10:03:12 +0000

    2025.10.01 - 팀 결성

---
+++ Python/ndb/graph_theory/팀_결성.py
@@ -0,0 +1,31 @@
+import sys
+input=sys.stdin.readline
+
+def find_parent(parent,x):
+    if(parent[x]!=x):
+        parent[x]=find_parent(parent,parent[x])
+    return parent[x]
+
+def union_parent(parent,a,b):
+    a=find_parent(parent,a)
+    b=find_parent(parent,b)
+    
+    if(a<b):
+        parent[b]=a
+    else:
+        parent[a]=b
+
+n,m=map(int,input().split())
+parent=[i for i in range(n+1)]
+
+for _ in range(m):
+    c,a,b=map(int,input().split())
+    if(c==0):
+        union_parent(parent,a,b)
+    else:
+        a=find_parent(parent,a)
+        b=find_parent(parent,b)
+        if(a==b):
+            print("YES")
+        else:
+            print("NO")
