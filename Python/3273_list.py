1309804d97029970e1fc3e1b88c778b55afd5c88
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-10-09 06:46:23 +0000

    2025.10.09 - 3273 "두 수의 합", list 풀이, 수정

--- Python/3273_list.py
+++ Python/3273_list.py
@@ -9,16 +9,19 @@ x=int(input())
 res=0
 
 for num in lst:
-    cnt[num]+=1
+    cnt[num]=1
 
 for num in lst:
-    cnt[num]-=1
-    if(x-num<=0):#이 조건문이 없어도 정답 처리는 된다..
+    cnt[num]=0
+    if(x-num<=0):
         continue 
     if(cnt[x-num]>0):
-        cnt[x-num]-=1
+        cnt[x-num]=0
         res+=1
 
 print(res)
 
 
+#수정 
+#수가 중복되는 경우가 없기 때문에 카운트를 계산할 필요가 없음.
+#있는지 없는지만 판단하면 됨.
