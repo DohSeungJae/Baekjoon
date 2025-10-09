39532755d9c49fa7579c98799f5e6f8387cc6568
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-10-09 06:46:01 +0000

    2025.10.09 - 3273 "두 수의 합", hash 풀이, 수정

--- Python/3273_hash.py
+++ Python/3273_hash.py
@@ -10,16 +10,17 @@ res=0
 for n in array:
     if not n in cnt.keys():
         cnt[n]=1
-    else:
-        cnt[n]+=1
-
+        
 for n in array:
-    cnt[n]-=1
+    cnt[n]=0
     if((x-n) in cnt.keys()):
-        if(cnt[x-n]>0):
-            cnt[x-n]-=1
+        if(cnt[x-n]==1):
+            cnt[x-n]=0
             res+=1
 
 print(res)
 
+#수정 
+#수가 중복되는 경우가 없기 때문에 카운트를 계산할 필요가 없음.
+#있는지 없는지만 판단하면 됨.
             
